185077c535b719078c49f5afd8ade477990c3fcc
stephan krebernik <sk@datatainment.de>
2026-03-03 17:38:06 +0100

    Ensure consistent naming for Homebridge plugin configuration • Changed the accessory name in api.registerAccessory from 'homebridge-garage-control' to 'GarageDoorControl' in index.js. • Set pluginAlias in config.schema.json to 'GarageDoorControl'. • Updated the Homebridge configuration (config.json) to use "accessory": "GarageDoorControl". • Ensured that accessory name, pluginAlias, and configuration are consistent so the plugin is correctly recognized and marked as configured by Homebridge and the UI. These changes resolve the issue where the plugin was shown as "not configured" in the UI, even though it was working. nodejs 24 added.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const packageJson = require('./package.json');
 
 module.exports = (api) => {
-  api.registerAccessory('homebridge-garage-control', 'homebridge-garage-control', GarageDoorControl);
+  api.registerAccessory('GarageDoorControl', 'GarageDoorControl', GarageDoorControl);
 };
 
 class GarageDoorControl {
